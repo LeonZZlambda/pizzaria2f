a99a9e3df1270437992e7629c17c6988c8474b55
LeonZZlambda <leonzzlopes@proton.me>
2024-06-06 10:01:11 -0300

    save

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import logo from '../assets/react.svg'
+
+const Home = () => {
+
+
+    return (
+        <Link to="/inicio">
+            <img src={logo} width="300" alt="logo do React" />
+        </Link>
+    )
+}
+
+export default Home
